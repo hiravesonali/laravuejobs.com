9d970e1f1092f6db71e5f8157b88dbe8d0544c90
Sonali Hirave <hiravesonali@gmail.com>
2020-05-30 20:23:26 +0200

    tailwind ui installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
-  theme: {},
-  variants: {},
-  plugins: []
+    theme: {},
+    variants: {},
+    plugins: [
+        require('@tailwindcss/ui'),
+    ]
 }
