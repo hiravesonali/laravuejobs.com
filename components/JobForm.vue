87b63a94541e8f63a79c9ed4f8a1f95875904a30
Sonali Hirave <hiravesonali@gmail.com>
2020-06-05 22:13:53 +0200

    edit form page

--- components/JobForm.vue
+++ components/JobForm.vue
@@ -38,19 +38,35 @@
                   </div>
                 </div>
 
+                <div class="grid grid-cols-3 gap-6 mt-6">
+                  <div class="col-span-3 sm:col-span-2">
+                    <label
+                      for="company_website"
+                      class="block text-md font-medium leading-5 text-gray-700"
+                    >Salary</label>
+                    <div class="mt-1 flex rounded-md shadow-sm">
+                      <input
+                        id="company_website"
+                        class="form-input flex-1 block w-full rounded-md transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                        placeholder="80.000$-120.000$ / year"
+                      />
+                    </div>
+                  </div>
+                </div>
+
                 <div class="mt-6">
-                  <label for="about" class="block text-sm leading-5 font-medium text-gray-700">About</label>
+                  <label for="about" class="block text-sm leading-5 font-medium text-gray-700">Description</label>
                   <div class="rounded-md shadow-sm">
                     <textarea
-                      id="about"
-                      rows="3"
+                      id="description"
+                      rows="10"
                       class="form-textarea mt-1 block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
                       placeholder="you@example.com"
                     ></textarea>
                   </div>
                   <p
                     class="mt-2 text-sm text-gray-500"
-                  >Brief description for your profile. URLs are hyperlinked.</p>
+                  >Brief description for your profile.</p>
                 </div>
 
                 <div class="mt-6">
