005134781b706edb348964a1714bf468283eb6d8
Sonali Hirave <hiravesonali@gmail.com>
2020-06-08 20:21:38 +0200

    add REST API

--- components/JobList.vue
+++ components/JobList.vue
@@ -2,13 +2,22 @@
   <div>
     <div class="max-w-screen-xl mx-auto overflow-hidden sm:rounded-md">
       <ul>
-        <job-list-row />
-        <job-list-row />
-        <job-list-row />
-        <job-list-row />
-        <job-list-row />
-        <job-list-row />
+        <job-list-row v-for="job in jobs.data" :key="job.id" />
       </ul>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      jobs: []
+    }
+  },
+  async fetch () {
+    this.jobs = await this.$http.$get('http://api.laravuejobs.test/api/jobs')
+    console.log(this.jobs)
+  }
+}
+</script>
