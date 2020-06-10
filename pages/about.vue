458677d5c69b1bb0a9279d87764b68eaef30027c
Sonali Hirave <hiravesonali@gmail.com>
2020-06-10 22:23:08 +0200

    About content added

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,56 @@
 <template>
-  <div>
-    <h1>About</h1>
-  </div>
+  <div class="pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+      <div class="max-w-3xl mx-auto">
+        <div>
+          <h1
+            class="text-3xl leading-9 tracking-tight font-medium text-gray-900 sm:text-4xl sm:leading-10"
+          >About LaraVuejobs.com</h1>
+          <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >LaraVuejobs is the open job board created and maintained by <a class="text-indigo-600" href="https://shona-design.com/" target="_blank">Sonali Hirave</a>.</p>
+            <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >This idea hit me when I have started my <a class="text-green-500" href="https://shona-design.com/100-days-of-code" target="_blank">#100DaysOfCode</a> challenge few weeks back. I actually wanted to build some production ready and also worth the effort. So far I have really enjoyed and learned a lot with this project.</p>
+          <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >The technologies behind this project are <a class="text-green-500" href="https://nuxtjs.org/" target="_blank">NuxtJs</a>
+           and <a class="text-green-500" href="https://tailwindcss.com/" target="_blank">TailwindCSS</a>
+            for frontend, <a class="text-green-500" href="https://laravel.com/" target="_blank">Laravel</a>
+             and <a class="text-green-500" href="https://www.mysql.com/" target="_blank">MySql</a> for backend REST API. Frontend is hosted on
+             <a class="text-green-500" href="https://vercel.com/" target="_blank">Vercel</a> and backend is hosted on
+             <a class="text-green-500" href="https://www.hetzner.com/cloud" target="_blank">Hetzner cloud</a>. Here are
+             <a class="text-green-500" href="https://github.com/hiravesonali/laravuejobs.com" target="_blank">Frontend</a> and
+             <a class="text-green-500" href="https://github.com/LaraVueJobs/api.laravuejobs.com" target="_blank">Backend</a> code repositories.</p>
+          <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >The main focus for this job board is to collect worldwide trending jobs for technologies like <b>Laravel, VueJs, NuxtJs, TailwindCSS</b> and so on.</p>
+          <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >At the moment post job functionality is work in progress, in the mean time if you would like to post your job please write me on <a class="text-indigo-600" href="https://twitter.com/ShonaDesign" target="_blank">Twitter</a>. </p>
+          <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >Have any suggestions or questions? Please don't hesitate to write me message on twitter.</p>
+        </div>
+      </div>
+      <hr class="my-16 max-w-3xl mx-auto border-2 border-green-200">
+       <div class="max-w-3xl mx-auto">
+        <div>
+          <h2
+            class="text-2xl leading-9 tracking-tight font-medium text-gray-900 sm:text-3xl sm:leading-10"
+          >A little bit about myself.</h2>
+          <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >My self <a class="text-indigo-600" href="https://shona-design.com/" target="_blank">Sonali Hirave</a>, I'm a product designer &amp; ProCreator who is trying to make a little progress in development world. I'm currently based in Nuremberg, Germany.</p>
+          <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >In my past experience, I've worked on different business domains. I would like to get engaged with startups to wear multiple hats.</p>
+          <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >I'm passionate about improving the lives of others through what I do and I am constantly looking to learn new things everyday.</p>
+          <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >Feel free to contact me and have a great day!🙌</p>
+        </div>
+      </div>
+    </div>
 </template>
