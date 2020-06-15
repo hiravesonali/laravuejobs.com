0f562b638ff130733d59e12363afe047b008b4eb
Sonali Hirave <hiravesonali@gmail.com>
2020-06-15 13:07:45 +0200

    Google analytics added

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,12 @@ export default {
         // Doc: https://github.com/nuxt-community/eslint-module
         '@nuxtjs/eslint-module',
         // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-        '@nuxtjs/tailwindcss'
+        '@nuxtjs/tailwindcss',
+
+        ['@nuxtjs/google-analytics', {
+            id: 'UA-169367248-1'
+        }]
+
     ],
     /*
      ** Nuxt.js modules
