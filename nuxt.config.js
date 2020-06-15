51033a099604b974cc439f836fa25bb1eae9baad
Sonali Hirave <hiravesonali@gmail.com>
2020-06-15 14:43:20 +0200

    Added twitter SEO card

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,13 @@ export default {
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+            { name: 'twitter:card', content: 'summary_large_image' },
+            { name: 'twitter:site', content: '@shonaDesign' },
+            { name: 'twitter:creator', content: '@shonaDesign' },
+            { name: 'twitter:title', content: 'Make your career as a Fullstack Developer' },
+            { name: 'twitter:description', content: 'Curated list of worldwide jobs for Laravel, VueJs, NuxtJs and TailwindCSS stack. Subscribe here to get regular updates on the job postings.' },
+            { name: 'twitter:image', content: '/website.png' },
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
