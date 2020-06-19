ba36a1b51b61137a5d4123531e1e9a0a277de867
Sonali Hirave <hiravesonali@gmail.com>
2020-06-19 19:07:51 +0200

    fixed firefow scrolling issue

--- layouts/home.vue
+++ layouts/home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-body font-body overflow-scroll">
+  <div class="text-body font-body overflow-auto">
     <div class="relative bg-gray-50 overflow-hidden">
       <div class="relative pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
         <home-header />
